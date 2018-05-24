c37bad216c4db3373115cee3a569862d7c99ca6c
yepremyanadrian99 <yeranikoshamo@gmail.com>
2018-05-25 01:30:34 +0400

    added productByID, etc...

--- src/main/java/am/aca/shop/controller/MainController.java
+++ src/main/java/am/aca/shop/controller/MainController.java
@@ -1,10 +1,14 @@
 package am.aca.shop.controller;
 
+import am.aca.shop.domain.Product;
 import am.aca.shop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -24,6 +28,10 @@ public class MainController {
         return modelAndView;
     }
 
-
-
+    @RequestMapping("/product")
+    public ModelAndView product(@RequestParam(value = "id") int id) {
+        ModelAndView modelAndView = new ModelAndView("product");
+        modelAndView.addObject("product", productService.getProductById(id));
+        return modelAndView;
+    }
 }
